9d7162cb385fdc48bfbfa9e6339449ae0d7bbfbd
mateusz <mateusznieweglowski10@gmail.com>
2026-05-04 20:13:52 +0200

    added blackjack game

--- src/games/blackjack.py
+++ src/games/blackjack.py
@@ -1,3 +1,5 @@
+import time
+
 from src.games import Game
 from src.models import Card
 from src.services import DeckOfCards
@@ -5,21 +7,125 @@ from src.services import DeckOfCards
 class Blackjack(Game):
     def __init__(self, deck_of_cards: DeckOfCards):
         self.__deck_of_cards: DeckOfCards = deck_of_cards
+        self.__croupier: list[Card] = []
+        self.__player: list[Card] = []
+        self.__croupier_value: int = 0
+        self.__player_value: int = 0
 
     def play(self) -> float:
         self.__deck_of_cards.shuffle()
-        croupier = [self.__deck_of_cards.get_card(), self.__deck_of_cards.get_card()]
-        player = [self.__deck_of_cards.get_card(), self.__deck_of_cards.get_card()]
 
-        print(f"krupier:\n {croupier[0].get_figure()} ?")
-        print(f"ty:\n")
-        self.__print_cards(player)
+        self.__croupier = [self.__deck_of_cards.get_card(), self.__deck_of_cards.get_card()]
+        self.__croupier_value = self.__get_cards_value(self.__croupier)
 
-        print("akcje:")
-        print("1. dobierz karte")
-        print("2. zakoncz ture")
+        self.__player = [self.__deck_of_cards.get_card(), self.__deck_of_cards.get_card()]
+        self.__player_value = self.__get_cards_value(self.__player)
 
-    def __print_cards(self, cards: list[Card]):
-        for card in cards:
+        if self.__player_value == 21:
+            self.__print_cards()
+            print("blackjack!!!, wygrales :)")
+            return 2.5
+
+        self.__print_cards(True)
+
+        player_round = self.__player_round()
+        if player_round <= 0:
+            print("twoj wynik jest powyzej 21, przegrales :(")
+            return player_round
+
+        if self.__croupier_round():
+            if self.__player_value > self.__croupier_value:
+                print("wygrales :)")
+                return player_round
+            elif self.__player_value == self.__croupier_value:
+                print("remis :/")
+                return 1
+            elif self.__player_value < self.__croupier_value:
+                print("przegrales :(")
+                return 0
+
+        print("wygrales :)")
+        return player_round
+
+
+    def __player_round(self) -> float:
+        print("-------------------------------------------")
+        print("twoja runda")
+        print("-------------------------------------------")
+        choice = int(input("akcje: 1. dobierz karte | 2. zakoncz ture | 3. podwoj stawke\n"))
+        if choice == 3:
+            self.__player.append(self.__deck_of_cards.get_card())
+            self.__player_value = self.__get_cards_value(self.__player)
+        elif choice == 1:
+            while True:
+                self.__player.append(self.__deck_of_cards.get_card())
+                self.__player_value = self.__get_cards_value(self.__player)
+                if self.__player_value > 21:
+                    break
+
+                self.__print_cards(True)
+                choice = int(input("akcje: 1. dobierz karte | 2. zakoncz ture\n"))
+                if choice == 2:
+                    break
+
+        self.__print_cards()
+
+        if self.__player_value > 21:
+            if choice == 3:
+                return -1
+            return 0
+        else:
+            if choice == 3:
+                return 4
+            return 2
+
+
+    def __croupier_round(self) -> bool:
+        if self.__croupier_value >= 17:
+            return True
+        print("-------------------------------------------")
+        print("runda krupiera")
+        print("-------------------------------------------")
+        while self.__croupier_value < 17:
+            time.sleep(1.5)
+            self.__croupier.append(self.__deck_of_cards.get_card())
+            self.__croupier_value = self.__get_cards_value(self.__croupier)
+            self.__print_cards()
+
+        return self.__croupier_value <= 21
+
+
+    def __print_cards(self, covered: bool = False) -> None:
+        print("-------------------------------------------")
+        if covered:
+            print(f"krupier: {self.__croupier[0].get_figure()} ? | punkty: {self.__get_cards_value(self.__croupier[0:1])}")
+        else:
+            print(f"krupier: ", end=" ")
+            for card in self.__croupier:
+                print(card.get_figure(), end=" ")
+            print(f"| punkty: {self.__croupier_value}")
+
+        print(f"ty: ", end=" ")
+        for card in self.__player:
             print(card.get_figure(), end=" ")
-        print("")
+        print(f"| punkty: {self.__player_value}")
+        print("-------------------------------------------")
+
+
+    def __get_cards_value(self, cards: list[Card]) -> int:
+        card_values: dict[str, int] = \
+            {"2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+        value = 0
+        aces = 0
+        for card in cards:
+            if card.get_figure() == "A":
+                    value += 11
+                    aces += 1
+            else:
+                value += card_values[card.get_figure()]
+
+        while value > 21 and aces > 0:
+            value -= 10
+            aces -= 1
+
+        return value
